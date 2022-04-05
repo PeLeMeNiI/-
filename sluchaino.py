acb1420483eda98c548eb32355cc4d3050bc3ee7
PeLeMeNiI <101660014+PeLeMeNiI@users.noreply.github.com>
2022-04-05 19:56:21 +0300

    Add files via upload

---
+++ sluchaino.py
@@ -0,0 +1,8 @@
+from random import randint
+from_1_to_10  =randint(1,10)
+from_10_to_100 = randint(10,100)
+from_100_to_1000 = randint(100,1000)
+
+print(f'Random number between 1-10: {from_1_to_10}')
+print(f'Random number between 10-100: {from_10_to_100}')
+print(f'Random number between 100-1000: {from_100_to_1000}')
